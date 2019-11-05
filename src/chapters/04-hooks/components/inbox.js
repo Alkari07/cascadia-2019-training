775d2866d695fbe3530f0eb3767654546e01e344
AVWORLD\rob9575 <rsantos@esri.com>
2019-11-04 18:07:14 -0600

    saved the part of exercise 4 I got through successfully, with functional context hooks

--- src/chapters/04-hooks/components/inbox.js
+++ src/chapters/04-hooks/components/inbox.js
@@ -1,34 +1,58 @@
 import React from "react";
-import { AppConsumer } from "../context";
+import { AppContext } from "../context";
 import Denied from "./denied";
 import Empty from "./empty";
 import Preview from "./preview";
 import EmailError from "./email-error";
 
-const Inbox = () => (
-  <AppConsumer>
-    {({ isAuthenticated, emails, removeEmail }) => {
-      if (!isAuthenticated) {
-        return <Denied />;
-      }
+// const Inbox = () => (
+//    //const {isAuthenticated, email, login} = React.useContext(AppContext);
 
-      if (!emails.length) {
-        return <Empty />;
-      }
+//     if (!isAuthenticated) {
+//       return <Denied />;
+//     }
 
-      return (
-        <ul className="inbox">
-          {emails.map(email => {
-            return (
-              <EmailError key={email.id} onClear={() => removeEmail(email.id)}>
-                <Preview {...email} />
-              </EmailError>
-            );
-          })}
-        </ul>
-      );
-    }}
-  </AppConsumer>
-);
+//     if (!emails.length) {
+//       return <Empty />;
+//     }
+
+//     return (
+//       <ul className="inbox">
+//         {emails.map(email => {
+//           return (
+//             <EmailError key={email.id} onClear={() => removeEmail(email.id)}>
+//               <Preview {...email} />
+//             </EmailError>
+//           );
+//         })}
+//       </ul>
+//     );
+
+// );
+
+
+const Inbox = () => {
+  const {isAuthenticated, emails, removeEmail} = React.useContext(AppContext);
+
+    if (!isAuthenticated) {
+      return <Denied />;
+    }
+
+    if (!emails.length) {
+      return <Empty />;
+    }
+
+    return (
+      <ul className="inbox">
+        {emails.map(email => {
+          return (
+            <EmailError key={email.id} onClear={() => removeEmail(email.id)}>
+              <Preview {...email} />
+            </EmailError>
+          );
+        })}
+      </ul>
+    );
+}
 
 export default Inbox
--- src/chapters/04-hooks/components/navbar.js
+++ src/chapters/04-hooks/components/navbar.js
@@ -1,28 +1,26 @@
 import React from "react";
 import { Link } from "@reach/router";
-import { AppConsumer } from "../context";
+import { AppContext } from "../context";
 
 export default function NavBar() {
+  const {isAuthenticated, logout, login} = React.useContext(AppContext);
+  
   return (
-    <AppConsumer>
-      {({ isAuthenticated, login, logout }) => (
-        <nav className="navbar">
-          <Link to={"/"}>
-            <h1 className="nav-logo">Formidamail</h1>
-          </Link>
-          <ul className="nav-items">
-            {isAuthenticated ? (
-              <li className="nav-item" onClick={logout}>
-                logout
-              </li>
-            ) : (
-              <li className="nav-item" onClick={login}>
-                Log in
-              </li>
-            )}
-          </ul>
-        </nav>
-      )}
-    </AppConsumer>
+    <nav className="navbar">
+      <Link to={"/"}>
+        <h1 className="nav-logo">Formidamail</h1>
+      </Link>
+      <ul className="nav-items">
+        {isAuthenticated ? (
+          <li className="nav-item" onClick={logout}>
+            logout
+          </li>
+        ) : (
+          <li className="nav-item" onClick={login}>
+            Log in
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 }
